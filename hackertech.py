788118c5a032adf7e1fbc9e60b8d78a68b64885b
Zacker <Zacker>
2017-12-16 15:03:29 +0530

    initial commit

---
+++ hackertech.py
@@ -0,0 +1,39 @@
+import tornado
+import tornado.ioloop
+import tornado.web
+import os, uuid
+from image_process import dirty_percentage
+
+
+__UPLOADS__ = "./uploads/"
+
+class Userform(tornado.web.RequestHandler):
+    def get(self):
+        self.render("form.html")
+
+
+class Upload(tornado.web.RequestHandler):
+    def post(self):
+        fileinfo = self.request.files['filearg'][0]
+        print "fileinfo is", fileinfo
+        fname = fileinfo['filename']
+        extn = os.path.splitext(fname)[1]
+        cname = str(uuid.uuid4()) + extn
+        fh = open(__UPLOADS__ + cname, 'wb')
+        fh.write(fileinfo['body'])
+        print fileinfo['body']
+        
+        result=dirty_percentage("uploads/"+cname)
+
+        self.finish(result)
+
+
+application = tornado.web.Application([
+        (r"/", Userform),
+        (r"/upload", Upload),
+        ], debug=False)
+
+
+if __name__ == "__main__":
+    application.listen(5000)
+    tornado.ioloop.IOLoop.instance().start()
